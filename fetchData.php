3115e81dde137db3f9b5455845795b04553ce44b
Tom Dirinck <tom.dirinck@gmail.com>
2021-05-19 17:24:44 +0200

    Fetch evolution urls & cleanup code.

--- fetchData.php
+++ fetchData.php
@@ -4,7 +4,6 @@ function getData()
     $pokemon = $_GET['pokemon'];
     $data = file_get_contents("https://pokeapi.co/api/v2/pokemon/$pokemon");
     $dataSpecies = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/$pokemon");
-    // $evolutionChain = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/$pokemon");
     $result1 = json_decode($data, true);
     $result2 = json_decode($dataSpecies, true);
     $result = array_merge($result1, $result2);
@@ -22,35 +21,39 @@ function getData()
     $type = $result['types'][0]['type']['name'];
     $sprite = $result['sprites']['front_default'];
 
-    // var_dump($chainData['chain']['evolves_to'][0]['species']);
     $chain = [];
     $chain = getChainNames(($chainData['chain']));
-    var_dump($chain);
 
+    $evoChain = fetchChainUrls($chain);
 
-    $poke = new Pokemon($name, $health, $power, $type, $height, $weight, $abilities, $sprite);
-
-    // var_dump($chainData['chain']['evolves_to'][0]['evolves_to'][0]['species']['name']);
-    // var_dump($poke->abilities);
-    // var_dump($result['sprites']['front_default']);
-    // foreach ($poke->abilities as $move) {
-    //     var_dump($move['move']['name']);
-    // }
+    $poke = new Pokemon($name, $health, $power, $type, $height, $weight, $abilities, $sprite, $evoChain);
     return $poke;
 }
 
 
-    function getChainNames($chain)
-    {
-        $names = [];
-        foreach ($chain as $key => $value) {
-            if ($key === 'species') {
-                array_push($names, $value['name']);
-            };
-            if ($key === 'evolves_to' && !empty($value)) {
-                $result = getChainNames($value[0]);
-                $names = array_merge($names, $result);
-            }
+function getChainNames($chain)
+{
+    $names = [];
+    foreach ($chain as $key => $value) {
+        if ($key === 'species') {
+            array_push($names, $value['name']);
+        };
+        if ($key === 'evolves_to' && !empty($value)) {
+            $result = getChainNames($value[0]);
+            $names = array_merge($names, $result);
         }
-        return $names;
     }
+    return $names;
+}
+
+function fetchChainUrls($names)
+{
+    $urls = [];
+    foreach ($names as $name) {
+        $data = file_get_contents("https://pokeapi.co/api/v2/pokemon/$name");
+        $result = json_decode($data, true);
+        $sprite = $result['sprites']['front_default'];
+        array_push($urls, $sprite);
+    }
+    return $urls;
+}
